1552adde3da6c45492fd524312e6c01f7b26ee96
Petar Todorov <petar@uchange.co>
2017-07-10 14:43:59 +0200

    pepeight the whitespace

--- WikidataSPARQLwrapper.py
+++ WikidataSPARQLwrapper.py
@@ -30,22 +30,22 @@ class WikidataTreeBuilder(object):
 
 	def fromRoot(self,root,forbidden):
 		"""Build a tree from root (specify root with a tuple, ("Q####","name") """
-		return self.makeTree(root,[],forbidden)
+		return self.makeTree(root, [], forbidden)
 
 	def queryWikidata(self,P,Q):
 		"""Form and execute the wikidata query"""
-		query = self.queryTemplate.format(P,Q,self._labelsLanguages)
+		query = self.queryTemplate.format(P, Q, self._labelsLanguages)
 		if self._debug:
 			print(query)
 		page = self.session.get(self.queryPrefix+query)
-		if page.status_code!=200:
+		if page.status_code != 200:
 			print("ERROR WHILE RETRIEVING "+P+" OF "+str(Q)+". STATUS:"+str(page.status_code))
 			return []
 		result=json.loads(page.text)
 		# Provide a simplified version of the result object
 		return [(item["entityId"]["value"],item["entityLabel"]["value"]) for item in result["results"]["bindings"]]
 
-	def makeTree(self,node,visited,forbidden):
+	def makeTree(self, node, visited, forbidden):
 		"""Recursive function to explore the tree"""
 
 		flare = dict ()
@@ -55,25 +55,25 @@ class WikidataTreeBuilder(object):
 			flare["visitedNodes"] = visited
 
 		#Query wikidata for all instanceof (P31) and subclassof (P279)
-		queryResult = {P:self.queryWikidata(P,node[0]) for P in self._queryProperties}
+		queryResult = {P:self.queryWikidata(P, node[0]) for P in self._queryProperties}
 		result = queryResult["P31"] + queryResult["P279"]
 
 		if len(result)==0:
 			return flare
 
 		# recursively call the function for all subclasses and instances
-		flare["children"] = [self.makeTree(entry,visited+[node[0]],forbidden) for entry in result if not entry[0] in visited and not entry[0] in forbidden]
+		flare["children"] = [self.makeTree(entry, visited+[node[0]], forbidden) for entry in result if not entry[0] in visited and not entry[0] in forbidden]
 
 		# Optional : this part puts all entries that are not subclasses into a subnode called "singleEntries". For viz.
-		newlyStructured = list ()
-		singleEntries = list ()
+		newlyStructured = list()
+		singleEntries = list()
 		for index, entry in enumerate(flare["children"]):
 			if "children" in entry.keys():
 				newlyStructured.append(entry)
 			else:
 				singleEntries.append(entry)
 
-		newlyStructured.append({"name":["-1","singleEntries"],"children":singleEntries})
+		newlyStructured.append({"name":["-1","singleEntries"], "children":singleEntries})
 		flare["children"] = newlyStructured
 
 		return flare
@@ -82,7 +82,7 @@ def main():
 	"""Example run"""
 	tree = WikidataTreeBuilder()
 	print(datetime.now())
-	flare = tree.fromRoot(root=("Q21198","Computer Science"),forbidden=["Q7889","Q28923"])
+	flare = tree.fromRoot(root=("Q21198","Computer Science"), forbidden=["Q7889","Q28923"])
 	print(datetime.now())
 	with open("output.json","w") as f:
 		json.dump(flare,f,indent=4)
